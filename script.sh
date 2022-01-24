924cce5fb71d86ce465eb07082af2310ea348a89
Gaurav Rajendra Girase <78299809+GauravGirase@users.noreply.github.com>
2022-01-24 10:35:46 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -7,4 +7,5 @@ pip install flake8-junit-report
 pip install pytest
 flake8 --format=pylint calculator/ >flake8-out.txt
 flake8_junit flake8-out.txt flake8_junit.xml
+py.test --junit-xml test-reports/results.xml mytests/
 python --version
