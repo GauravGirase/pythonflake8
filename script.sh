dea71bf89ad7662a6f80cb910c89827e9c298790
Gaurav Rajendra Girase <78299809+GauravGirase@users.noreply.github.com>
2022-01-24 10:10:59 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -3,5 +3,7 @@ VENV="venv"
 virtualenv $VENV
 source $VENV/bin/activate
 pip install flake8
+pip install flake8-junit-report
 flake8 --format=pylint calculator/ >flake8-out.txt
+flake8_junit flake8.txt flake8_junit.xml
 python --version
