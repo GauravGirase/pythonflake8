117f494a2056904250a7c1f1eeac0ee4785e1238
Gaurav Rajendra Girase <78299809+GauravGirase@users.noreply.github.com>
2022-01-24 10:41:22 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -8,5 +8,5 @@ pip install flake8-junit-report
 pip install pytest
 flake8 --format=pylint calculator/ >flake8-out.txt
 flake8_junit flake8-out.txt flake8_junit.xml
-py.test --junit-xml test-reports/results.xml mytests/
-python --version
+py.test --junit-xml test-reports/results.xml mytests/test_SimpleCalc.py
+deactivate
